068e6f3c6b2641e450b363b775f5c8e1a2caa1d0
Keith Tan <keith_xw_tan@tech.gov.sg>
2023-01-06 15:02:04 +0800

    [MISC][KT] Enhance story to have styling information

---
+++ stories/icon/demo.tsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+import { ArrowDownIcon } from "../../src/arrow-down";
+
+export const MyCustomIcon = styled(ArrowDownIcon)`
+    color: red;
+    height: 2rem;
+    width: 2rem;
+`;
