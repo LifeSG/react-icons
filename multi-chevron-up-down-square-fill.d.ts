221d6174e909ee5f0dd64ee4792913711a4074e5
group_7001_bot_1 <group_7001_bot_1@noreply.sgts.gitlab-dedicated.com>
2026-07-08 18:44:27 +0800

    Built package | Version: v1.19.0 | Gitlab: react-icons main 21029644

---
+++ multi-chevron-up-down-square-fill.d.ts
@@ -0,0 +1,5 @@
+import { SVGProps } from "react";
+export declare const MultiChevronUpDownSquareFillIcon: {
+    (props: SVGProps<SVGSVGElement>): import("react/jsx-runtime").JSX.Element;
+    displayName: string;
+};
---
+++ unlink.d.ts
@@ -0,0 +1,5 @@
+import { SVGProps } from "react";
+export declare const UnlinkIcon: {
+    (props: SVGProps<SVGSVGElement>): import("react/jsx-runtime").JSX.Element;
+    displayName: string;
+};
