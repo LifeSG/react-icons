d7ab4308849fbe64d6a8f827656d2ff40dcee500
group_7001_bot_1 <group_7001_bot_1@noreply.sgts.gitlab-dedicated.com>
2026-09-21 18:04:55 +0800

    Built package | Version: v1.22.0 | Gitlab: react-icons main 23954905

---
+++ square-2x2-plus-fill.d.ts
@@ -0,0 +1,5 @@
+import { SVGProps } from "react";
+export declare const Square2x2PlusFillIcon: {
+    (props: SVGProps<SVGSVGElement>): import("react/jsx-runtime").JSX.Element;
+    displayName: string;
+};
